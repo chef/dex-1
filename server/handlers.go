4f1fe05d57c38542707e40b5a6d6722820796809
SanjuPal01 <sanju.sanju@progress.com>
2022-08-22 16:04:34 +0530

    Check

--- server/handlers.go
+++ server/handlers.go
@@ -928,6 +928,7 @@ func (s *Server) sendCodeResponse(w http.ResponseWriter, r *http.Request, authRe
 }
 
 func (s *Server) handleToken(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello HandleToken")
 	clientID, clientSecret, ok := r.BasicAuth()
 	if ok {
 		var err error
